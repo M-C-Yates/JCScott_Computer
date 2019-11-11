eafbd3cf9a7dba8f1fda7906cb036f3ba364a5bc
Matthew Yates <Mat.C.Yates@gmail.com>
2019-11-11 14:00:51 -0500

    added enabler component

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
-import Byte from './circuit/Byte';
+import Enabler from './components/Enabler';
 
-const byte = new Byte();
-
-const byteIn = [true, false, false, true, false, true, true, false];
-const byteIn2 = [false, false, false, false, false, false, false, false];
-const byteIn3 = [true, false, false, true, false, true, true, false];
-
-byte.update(byteIn, true);
-byte.update(byteIn2, false);
-console.log(byte.get());
-byte.update(byteIn2, true);
-byte.update(byteIn3, false);
-console.log(byte.get());
+const enabler = new Enabler();
+const enabler2 = new Enabler();
+enabler.connectOutput(enabler2);
+enabler.setInputWire(0, true);
+enabler.setInputWire(1, true);
+enabler.setInputWire(2, true);
+enabler.setInputWire(3, true);
+enabler.update(true);
+console.log(enabler);

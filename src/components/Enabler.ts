27ce09dd0a75887ddfbef5f97d30c342acbe298b
Matthew Yates <Mat.C.Yates@gmail.com>
2019-11-12 16:56:25 -0500

    .

--- src/components/Enabler.ts
+++ src/components/Enabler.ts
@@ -1,40 +1,18 @@
-import And from '../circuit/Gates';
-import ComponentInterface from './Component';
-import Wire from '../circuit/Wire';
+import And from "../circuit/Gates";
+import ComponentInterface from "./Component";
+import Wire from "../circuit/Wire";
 
-class Enabler implements ComponentInterface {
-	private inputs: Wire[] = new Array(8).fill(new Wire('o', false));
-	private gates: And[] = new Array(8).fill(new And());
-	private outputs: Wire[] = new Array(8).fill(new Wire('o', false));
-	private next: ComponentInterface | null = null;
-	private value: boolean[] = new Array(8).fill(8);
-	constructor() {}
-	connectOutput = (component: ComponentInterface) => {
-		this.next = component;
-	};
-	getOutputWire = (index: number): boolean => {
-		return this.outputs[index].get();
-	};
-	getVal = () => {
-		return this.outputs.map((wire) => {
-			return wire.get();
-		});
-		// console.log(result);
-	};
-	setInputWire = (index: number, value: boolean): void => {
-		this.inputs[index].set(value);
-	};
-	update = (enable: boolean) => {
-		for (let i = 0; i < 8; i++) {
-			this.gates[i].update(this.inputs[i].get(), enable);
-			this.outputs[i].set(this.gates[i].get());
-		}
-		if (this.next != null) {
-			for (let i = 0; i < 8; i++) {
-				this.next.setInputWire(i, this.outputs[i].get());
-			}
-		}
-	};
+class Enabler /* implements ComponentInterface */ {
+  private gates: And[] = new Array(8).fill(new And());
+  private outputs: Wire[] = new Array(8).fill(new Wire("o", false));
+  private next: ComponentInterface | null = null;
+
+  update = (inputs: boolean[], enable: boolean) => {
+    for (let i = 0; i < 8; i++) {
+      this.gates[i].update(inputs[i], enable);
+      this.outputs[i].set(this.gates[i].get());
+    }
+  };
 }
 
 export default Enabler;

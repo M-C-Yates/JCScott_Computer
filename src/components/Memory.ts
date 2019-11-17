db7a66c42af10944f8101b1b751fdff59b43da0b
Matthew Yates <Mat.C.Yates@gmail.com>
2019-11-17 10:55:42 -0500

    initial 256Byte memory finished

--- src/components/Memory.ts
+++ src/components/Memory.ts
@@ -26,11 +26,13 @@ class Cell {
   };
 }
 
-class Memory {
-  private decoderRow = new Decoder4x16();
-  private decoderCol = new Decoder4x16();
+class Memory256B {
   private addressRegister = new Register(this.inputBus, this.outputBus, "MAR");
+  private decoderCol = new Decoder4x16();
+  private decoderRow = new Decoder4x16();
+  private enable: boolean = true;
   private memory: Cell[][] = new Array(16).fill([]);
+  private set: boolean = true;
   constructor(private inputBus: Bus, private outputBus: Bus) {
     for (let i = 0; i < 16; i++) {
       for (let j = 0; j < 16; j++) {
@@ -38,6 +40,21 @@ class Memory {
       }
     }
   }
+  update = () => {
+    const byte = this.addressRegister.get();
+    this.decoderRow.update(byte[0], byte[1], byte[2], byte[3]);
+    this.decoderCol.update(byte[4], byte[5], byte[6], byte[7]);
+
+    const row = this.decoderRow.getIndex();
+    const col = this.decoderCol.getIndex();
+    this.memory[row][col].update(this.enable, this.set);
+  };
+  updateEnable = (enable: boolean) => {
+    this.enable = enable;
+  };
+  updateSet = (set: boolean) => {
+    this.set = set;
+  };
 }
 
-export default Memory;
+export default Memory256B;

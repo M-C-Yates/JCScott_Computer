476e16d02e82c4a756c219fbf9a0624b8c04396c
Matthew Yates <Mat.C.Yates@gmail.com>
2019-11-19 14:47:32 -0500

    implemented right shift tests

--- src/test/components/Shifters.test.ts
+++ src/test/components/Shifters.test.ts
@@ -5,7 +5,7 @@ describe("left shifter", () => {
   const bus = new Bus(8);
   const lShift = new leftShifter(bus, bus);
   const testByte = [true, false, false, false, false, false, false, true];
-  it("it should give correct output with shiftout not wrapping", () => {
+  it("it should give correct output with wrapping disabled", () => {
     bus.set(testByte);
     lShift.update(false);
     expect(bus.get()).toEqual([
@@ -19,7 +19,7 @@ describe("left shifter", () => {
       false
     ]);
   });
-  it("should give correct output with shiftOut being wrapped", () => {
+  it("should give correct output with wrapping enabled", () => {
     bus.set(testByte);
     lShift.update(true);
     expect(bus.get()).toEqual([
@@ -40,5 +40,32 @@ describe("right Shifter", () => {
   const rShift = new rightShifter(bus, bus);
   const testByte = [true, false, false, false, false, false, false, true];
 
-  it("should give the correct output", () => {});
+  it("should give the correct output with wrapping disabled", () => {
+    bus.set(testByte);
+    rShift.update(false);
+    expect(bus.get()).toEqual([
+      false,
+      true,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false
+    ]);
+  });
+  it("should give the correct output with wrapping enabled", () => {
+    bus.set(testByte);
+    rShift.update(true);
+    expect(bus.get()).toEqual([
+      true,
+      true,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false
+    ]);
+  });
 });

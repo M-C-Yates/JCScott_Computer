f5827bbe7cbf57092df9f063029df6ecb6655159
Matthew Yates <Mat.C.Yates@gmail.com>
2019-11-20 12:56:47 -0500

    further Components refactoring

--- src/components/Components.ts
+++ src/components/Components.ts
@@ -1,12 +1,24 @@
-import { And } from "./../circuit/Gates";
+import { And, Not } from "./../circuit/Gates";
 import Bus from "./Bus";
 class Notter {
+  private notGates = [
+    new Not(),
+    new Not(),
+    new Not(),
+    new Not(),
+    new Not(),
+    new Not(),
+    new Not(),
+    new Not()
+  ];
   private output = new Array(8).fill(false);
   constructor(private inputBus: Bus, private outputBus: Bus) {}
   update = () => {
     this.output = [...this.inputBus.get()];
+
     for (let i = 0; i < 8; i++) {
-      this.output[i] = !this.output[i];
+      this.notGates[i].update(this.output[i]);
+      this.output[i] = this.notGates[i].get();
     }
 
     this.outputBus.set([...this.output]);
@@ -38,16 +50,9 @@ export class Ander {
     this.data1 = [...this.inputA.get()];
     this.data2 = [...this.inputB.get()];
 
-    this.andGates[0].update(this.data1[0], this.data2[0]);
-    this.andGates[1].update(this.data1[1], this.data2[1]);
-    this.andGates[2].update(this.data1[2], this.data2[2]);
-    this.andGates[3].update(this.data1[3], this.data2[3]);
-    this.andGates[4].update(this.data1[4], this.data2[4]);
-    this.andGates[5].update(this.data1[5], this.data2[5]);
-    this.andGates[6].update(this.data1[6], this.data2[6]);
-    this.andGates[7].update(this.data1[7], this.data2[7]);
-
     for (let i = 0; i < 8; i++) {
+      this.andGates[i].update(this.data1[i], this.data2[i]);
+
       this.output[i] = this.andGates[i].get();
     }
 

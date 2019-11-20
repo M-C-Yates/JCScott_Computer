c4c38b337b97764796758d7809c0fcda67d94610
Matthew Yates <Mat.C.Yates@gmail.com>
2019-11-20 12:44:31 -0500

    implemented Ander

--- src/components/Components.ts
+++ src/components/Components.ts
@@ -12,4 +12,26 @@ class Notter {
   };
 }
 
+export class Ander {
+  private data1 = new Array(8).fill(false);
+  private data2 = new Array(8).fill(false);
+  private output = new Array(8).fill(false);
+
+  constructor(
+    private inputA: Bus,
+    private inputB: Bus,
+    private outputBus: Bus
+  ) {}
+
+  update = () => {
+    this.data1 = [...this.inputA.get()];
+    this.data2 = [...this.inputB.get()];
+    for (let i = 0; i < 8; i++) {
+      this.output[i] = this.data1[i] && this.data2[i];
+    }
+
+    this.outputBus.set([...this.output]);
+  };
+}
+
 export default Notter;
--- src/test/components/Components.test.ts
+++ src/test/components/Components.test.ts
@@ -1,7 +1,8 @@
+import { Ander } from "./../../components/Components";
 import Notter from "../../components/Components";
 import Bus from "../../components/Bus";
 
-describe("", () => {
+describe("Notter", () => {
   const bus = new Bus(8);
   const notter = new Notter(bus, bus);
   const testByte = new Array(8).fill(false);
@@ -21,6 +22,30 @@ describe("", () => {
       true,
       false,
       true,
+      false,
+      true
+    ]);
+  });
+});
+
+describe("Ander", () => {
+  const busA = new Bus(8);
+  const busB = new Bus(8);
+  const busC = new Bus(8);
+  const ander = new Ander(busA, busB, busC);
+  const testByte1 = [false, true, true, false, false, true, false, true];
+  const testByte2 = [true, true, false, true, false, false, true, true];
+  it("should give the correct output", () => {
+    busA.set(testByte1);
+    busB.set(testByte2);
+    ander.update();
+    expect(busC.get()).toEqual([
+      false,
+      true,
+      false,
+      false,
+      false,
+      false,
       false,
       true
     ]);

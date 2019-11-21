ce86767e5f3e079042e5cec0eae96e192bfbb5fe
Matthew Yates <Mat.C.Yates@gmail.com>
2019-11-21 13:53:11 -0500

    implemented Comparator

--- src/test/components/Comparator.test.ts
+++ src/test/components/Comparator.test.ts
@@ -1,4 +1,5 @@
 import Comparator, { bitComparer } from "./../../components/Comparator";
+import Bus from "../../components/Bus";
 
 describe("bitComparer", () => {
   const comparer = new bitComparer();
@@ -24,7 +25,65 @@ describe("bitComparer", () => {
   });
 });
 
-// describe("Comparator", () => {
-//   const comparer = new Comparator();
-//   it("should give correct output", () => {});
-// });
+describe("Comparator", () => {
+  const busA = new Bus(8);
+  const busB = new Bus(8);
+  const busC = new Bus(8);
+
+  const comparer = new Comparator(busA, busB, busC);
+
+  it("should give correct output", () => {
+    const byte1 = [false, false, false, false, false, false, false, false];
+    const byte2 = [false, false, false, false, false, false, false, false];
+
+    busA.set(byte1);
+    busB.set(byte2);
+    comparer.update();
+    expect(comparer.getEqual()).toBe(true);
+    expect(comparer.getLarger()).toBe(false);
+  });
+
+  it("should give correct output", () => {
+    const byte1 = [false, false, false, false, false, false, false, true];
+    const byte2 = [false, false, false, false, false, false, false, false];
+
+    busA.set(byte1);
+    busB.set(byte2);
+    comparer.update();
+    expect(comparer.getEqual()).toBe(false);
+    expect(comparer.getLarger()).toBe(true);
+  });
+
+  it("should give correct output", () => {
+    const byte1 = [false, false, false, false, false, false, false, false];
+    const byte2 = [false, false, false, false, false, false, false, true];
+
+    busA.set(byte1);
+    busB.set(byte2);
+    comparer.update();
+    expect(comparer.getEqual()).toBe(false);
+    expect(comparer.getLarger()).toBe(false);
+  });
+
+  it("should give correct output", () => {
+    const byte1 = [false, false, false, true, true, false, false, true];
+    const byte2 = [false, false, false, true, true, false, false, true];
+
+    busA.set(byte1);
+    busB.set(byte2);
+    comparer.update();
+    expect(comparer.getEqual()).toBe(true);
+    expect(comparer.getLarger()).toBe(false);
+  });
+
+  it("should give correct output", () => {
+    const byte1 = [false, false, false, true, true, false, false, true]; // 25
+    const byte2 = [false, false, false, true, true, false, false, true]; // 25
+
+    busA.set(byte1);
+    busB.set(byte2);
+    comparer.update();
+    expect(comparer.getEqual()).toBe(true);
+    expect(comparer.getLarger()).toBe(false);
+  });
+});

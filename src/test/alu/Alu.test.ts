e50a4ea786b31da84135c4c4b0afb969351fd65b
Matthew Yates <Mat.C.Yates@gmail.com>
2019-11-21 17:24:09 -0500

    updated tests

--- src/test/alu/Alu.test.ts
+++ src/test/alu/Alu.test.ts
@@ -1,10 +1,12 @@
 import Alu from "../../alu/Alu";
 import Bus from "../../components/Bus";
 
-// describe("Alu", () => {
-//   const busA = new Bus(8);
-//   const busB = new Bus(8);
-//   const busC = new Bus(8);
+describe("Alu", () => {
+  const busA = new Bus(8);
+  const busB = new Bus(8);
+  const outputBus = new Bus(8);
+  const flagBus = new Bus(8);
 
-//   const alu = new Alu(busA, busB, busC);
-// });
+  const alu = new Alu(busA, busB, outputBus, flagBus);
+  it("should have correct output", () => {});
+});

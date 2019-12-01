d4aa79f30be3804e7732dc6de5efcf5fa3a9ec75
Matthew Yates <Mat.C.Yates@gmail.com>
2019-12-01 13:03:59 -0500

    updated cpu

--- src/cpu/Cpu.ts
+++ src/cpu/Cpu.ts
@@ -1,17 +1,17 @@
 import Register from "../components/Register";
 import Bus from "../components/Bus";
 import Memory256B from "../memory/Memory";
+import BusOne from "../components/BusOne";
+import Alu from "../alu/Alu";
 
 class Cpu {
   private accBus = new Bus(8);
   private addressBus = new Bus(8);
   private aluBus = new Bus(8);
   private aluToFlagBus = new Bus(8);
-  private busOne = new Bus(8);
   private controlBus = new Bus(8);
   private ioBus = new Bus(8);
   private flagBus = new Bus(8);
-  private mainBus = new Bus(8);
   private tmpBus = new Bus(8);
 
   // general purpose registers
@@ -20,14 +20,16 @@ class Cpu {
   private register3 = new Register(this.mainBus, this.mainBus, "gp three");
   private register4 = new Register(this.mainBus, this.mainBus, "gp four");
 
+  private busOne = new BusOne(this.tmpBus, this.tmpBus);
   private tmpReg = new Register(this.mainBus, this.tmpBus, "tmp");
   private accReg = new Register(this.accBus, this.mainBus, "acc");
   private flagsReg = new Register(this.aluToFlagBus, this.flagBus, "flags");
-  private irReg = new Register(this.mainBus, this.controlBus, "ir");
-  private iarREG = new Register(this.mainBus, this.mainBus, "iar");
-  constructor(private memory: Memory256B) {
+
+  // components
+  private alu = new Alu(this.aluBus, this.tmpBus, this.accBus, this.flagBus);
+
+  constructor(private mainBus: Bus) {
     const initVal = new Array(8).fill(false);
-    this.irReg.disable();
     this.flagsReg.enable();
     this.tmpReg.enable();
     this.flagsReg.update(initVal);
